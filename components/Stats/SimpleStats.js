5940c74a80afe376fef0511fea61b4494ab18755
emilyyleung <emily_y_leung@outlook.com>
2022-09-24 14:06:03 +1000

    removed unnecessary

--- components/Stats/SimpleStats.js
+++ components/Stats/SimpleStats.js
@@ -1,9 +1,3 @@
-const stats = [
-  { name: 'Total Subscribers', stat: '71,897' },
-  { name: 'Avg. Open Rate', stat: '58.16%' },
-  { name: 'Avg. Click Rate', stat: '24.57%' },
-];
-
 export default function SimpleStats({ data }) {
   return (
     <div className="min-w-full">
